845e818785bf0ec647e7fb957e9362341040f5e1
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-09-14 15:28:01 +0300

    added map with react native maps

--- driver/src/screens/OrdersScreen/index.js
+++ driver/src/screens/OrdersScreen/index.js
@@ -1,16 +1,51 @@
 import { useRef, useMemo } from "react";
-import { View, Text, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Dimensions,
+  useWindowDimensions,
+} from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import orders from "../../../assets/data/orders.json";
 import OrderItem from "../../components/OrderItem";
+import MapView, { Marker } from "react-native-maps";
+import { Entypo } from "@expo/vector-icons";
 
 const OrdersScreen = () => {
   const bottomSheetRef = useRef(null);
+  const { width, height } = useWindowDimensions();
 
   const snapPoints = useMemo(() => ["12%", "95%"], []);
 
   return (
     <View style={{ backgroundColor: "lightblue", flex: "1" }}>
+      <MapView
+        style={{
+          height,
+          width,
+        }}
+        showsUserLocation
+        followsUserLocation
+      >
+        {orders.map((order) => (
+          <Marker
+            key={order.id}
+            coordinate={{
+              latitude: order.Restaurant.lat,
+              longitude: order.Restaurant.lng,
+            }}
+            title={order.Restaurant.name}
+            description={order.Restaurant.address}
+          >
+            <View
+              style={{ backgroundColor: "green", padding: 5, borderRadius: 20 }}
+            >
+              <Entypo name="shop" size={24} color="white" />
+            </View>
+          </Marker>
+        ))}
+      </MapView>
       <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
         <View style={{ alignItems: "center", marginBottom: 30 }}>
           <Text
